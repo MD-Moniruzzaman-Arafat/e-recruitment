fc6c2b3c18d9adee9a1e2ffde44498876c7da511
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-06-29 14:54:22 +0600

    Navbar and HomePageBanner Componentdesign

---
+++ src/components/HomePageBanner/HomePageBanner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import bannerBg from '../../assets/banner_bg.png'
+import bannerImage from '../../assets/banner_image.png'
+
+export default function HomePageBanner() {
+    return (
+        <>
+            <div className='max-w-[1728px] h-[712px] mx-auto flex flex-col md:flex-row justify-around items-center p-4 bg-white shadow-md' style={{ backgroundImage: ` url(${bannerBg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                <div className='max-w-[600px] text-white'>
+                    <h1 className='text-[40px] font-bold'>Welcome To</h1>
+                    <h1 className='text-[40px] font-bold'>Ha-meem Group</h1>
+                    <p className='text-[16px] font-normal max-w-[580px] my-4'>Designed to streamline our hiring process, this platform enables you to explore job openings, submit applications effortlessly, and track your progress—all in one place. Join us in shaping the future with innovation, passion, and excellence.</p>
+                    <input type="text" name="search" id="Search" placeholder='Job Search ...' className='bg-white text-[14px] p-2 w-[300px] text-gray-700 mb-4' />
+                    <p>Jobs Category : <span className='border-2 px-4 py-1 rounded-4xl'>IT</span> <span className='border-2 px-4 py-1 rounded-4xl'>HR</span> <span className='border-2 px-4 py-1 rounded-4xl'>...</span></p>
+                </div>
+                <div>
+                    <img src={bannerImage} alt="" />
+                </div>
+            </div >
+        </>
+    )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import logo from '../../assets/logo.jpg';
+
+export default function Navbar() {
+
+    return (
+        <>
+            <div className='md:fixed top-0 left-0 right-0 z-50 bg-transparent'>
+                <div className='max-w-[1728px] mx-auto flex flex-col md:flex-row justify-between items-center p-4'>
+                    <div className='flex items-center gap-2'>
+                        <img src={logo} alt="Logo" className='w-[60px] h-[43px]' />
+                        <div className='md:text-white'>
+                            <h1 className='text-[20px] md:text-[24px] font-bold'>E-Recruitment</h1>
+                            <p className='text-[10px] md:text-[12px] font-medium'>Streamlining Talent Acquisition with Technology</p>
+                        </div>
+                    </div>
+                    <div>
+                        <nav className='md:p-4 mt-2 md:mt-0'>
+                            <ul className='flex flex-wrap md:flex-nowrap md:text-white'>
+                                <li className='text-[16px] font-medium  cursor-pointer px-3'>Job Circular</li>
+                                <li className='text-[16px] font-medium border-r-2 cursor-pointer px-3'>Start up</li>
+                                <li className='text-[16px] font-medium  cursor-pointer px-3'>Sign In</li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
